ee748404470f3fa1abe956ff43318056f7025f8a
x0lotl <danil200319771@gmail.com>
2023-04-04 12:25:55 +0300

    memory error

--- ex-7/src/Main.java
+++ ex-7/src/Main.java
@@ -41,44 +41,54 @@ public class Main {
 
     scanner.close();
 
+    List<List<Integer>> configurations = new ArrayList<>();
+    generateConfigurations(configurations, new ArrayList<>(), K, N);
+    double maxReliability = 0.0;
     List<Integer> bestConfiguration = new ArrayList<>();
-    int[] configurationCounter = new int[1];
-    double[] maxReliability = new double[1];
-    int[] bestConfigurationIndex = new int[1];
-
-    generateConfigurations(bestConfiguration, new ArrayList<>(), elements, K, N, k1, k2, k3, maxReliability, configurationCounter, bestConfigurationIndex, 0);
+    int bestConfigurationIndex = 0;
+    for (int i = 0; i < configurations.size(); i++) {
+      List<Integer> configuration = configurations.get(i);
+      double reliability = calculateReliability(configuration, elements, k1, k2, k3);
+      if (reliability > maxReliability) {
+        maxReliability = reliability;
+        bestConfiguration = configuration;
+        bestConfigurationIndex = i;
+      }
+    }
 
     System.out.println("Задана структура: " + k1 + " " + k2 + " " + k3);
     System.out.println("Кількість різнотипних елементів: " + K);
-    System.out.println("Кількість різних конфігурацій: " + configurationCounter[0]);
-    System.out.println("Максимальна надійність: " + maxReliability[0]);
-    System.out.println("Конфігурація з максимальною надійністю (номер конфігурації): " + bestConfigurationIndex[0]);
+    System.out.println("Кількість різних конфігурацій: " + configurations.size());
+    System.out.println("Максимальна надійність: " + maxReliability);
+    System.out.println("Конфігурація з максимальною надійністю (номер конфігурації): " + (bestConfigurationIndex + 1));
+    System.out.print("Конфігурація з максимальною надійністю: ");
+    for (int i : bestConfiguration) {
+      System.out.print(i + " ");
+    }
+    System.out.println();
 
     PrintWriter outputFile = new PrintWriter("output.txt");
     outputFile.println("Задана структура: " + k1 + " " + k2 + " " + k3);
     outputFile.println("Кількість різнотипних елементів: " + K);
-    outputFile.println("Кількість різних конфігурацій: " + configurationCounter[0]);
-    outputFile.println("Максимальна надійність: " + maxReliability[0]);
-    outputFile.println("Конфігурація з максимальною надійністю (номер конфігурації): " + bestConfigurationIndex[0]);
+    outputFile.println("Кількість різних конфігурацій: " + configurations.size());
+    outputFile.println("Максимальна надійність: " + maxReliability);
+    outputFile.println("Конфігурація з максимальною надійністю (номер конфігурації): " + (bestConfigurationIndex + 1));
+    outputFile.print("Конфігурація з максимальною надійністю: ");
+    for (int i : bestConfiguration) {
+      outputFile.println(i + " ");
+    }
     outputFile.close();
-  }
+    }
 
-  private static void generateConfigurations(List<Integer> bestConfiguration, List<Integer> current, List<Element> elements, int K, int N, int k1, int k2, int k3, double[] maxReliability, int[] configurationCounter, int[] bestConfigurationIndex, int currentConfigurationIndex) {
+  private static void generateConfigurations(List<List<Integer>> configurations, List<Integer> current, int K, int N) {
     if (current.size() == N) {
-      configurationCounter[0]++;
-      double reliability = calculateReliability(current, elements, k1, k2, k3);
-      if (reliability > maxReliability[0]) {
-        maxReliability[0] = reliability;
-        bestConfigurationIndex[0] = currentConfigurationIndex;
-        bestConfiguration.clear();
-        bestConfiguration.addAll(current);
-      }
+      configurations.add(new ArrayList<>(current));
       return;
     }
 
     for (int i = 1; i <= K; i++) {
       current.add(i);
-      generateConfigurations(bestConfiguration, current, elements, K, N, k1, k2, k3, maxReliability, configurationCounter, bestConfigurationIndex, currentConfigurationIndex + 1);
+      generateConfigurations(configurations, current, K, N);
       current.remove(current.size() - 1);
     }
   }
@@ -97,6 +107,7 @@ public class Main {
       int elementType = configuration.get(i);
       R2 *= (1 - elements.get(elementType - 1).Qoj);
     }
+
     for (int i = k1 + k2; i < k1 + k2 + k3; i++) {
       int elementType = configuration.get(i);
       R3 *= (1 - elements.get(elementType - 1).Qoj);
@@ -104,5 +115,5 @@ public class Main {
 
     return R1 * R2 * R3;
   }
-}
 
+}
